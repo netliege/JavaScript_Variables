2b88405bfeffe5a6f64e9453d9cf26fd1f517ca1
netliege <netliege@gmail.com>
2021-07-22 14:23:12 -0400

    Completed

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Scott";
+let lastName = "Martin";
+var age = "30";
 
 // Exercise 2
+let fullName = firstName + " " + lastName; // Scott + " " + "Martin" => Scott Martin
+let templateFullName = `${firstName} ${lastName}`;
+console.log("fullName: ", fullName);
+console.log("templateFullName: ", templateFullName);
 
 // Exercise 3
+let myStory;
+let city = "Tampa";
+let pastime = "coding, gaming, biking, videogames and gym";
+
+myStory = `My name is ${fullName}. 
+I live in ${city}. 
+I like ${pastime}.`;
+
+console.log(myStory);
